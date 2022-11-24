968467186a0dd76885628fe6e8cda3cdd3808614
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2022-11-24 02:44:22 +0100

    fixed query db

--- db_stuff.sql
+++ db_stuff.sql
@@ -1,10 +1,10 @@
 create database auth_site;
 
 create table users (
-  	id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  	email varchar(319) NOT NULL,
- 	pass varchar(255) NOT NULL,
-   	type enum("user", "admin", "prisoner") DEFAULT "prisoner"
+	id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	email varchar(319) NOT NULL,
+	pass varchar(255) NOT NULL,
+	type enum("user", "admin", "prisoner") DEFAULT "prisoner"
 );
 
 
@@ -15,4 +15,4 @@ INSERT INTO users(id, email, pass, type) VALUES (NULL,'admin@admin.fr','admin98'
 INSERT INTO users(id, email, pass, type) VALUES (NULL,'tomy@hotmail.fr','produskate89',"user");
 
 -- PRSIONER USER
-INSERT INTO users(id, email, pass, type) VALUES (NULL,'groscaca@merde.fr','mangemerde09');
+INSERT INTO users(id, email, pass) VALUES (NULL,'groscaca@merde.fr','mangemerde09');
