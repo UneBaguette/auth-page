d0d000c5741440032df367c2c7e418c1b68bffbc
UneBaguette <28904802+UneBaguette@users.noreply.github.com>
2022-11-24 02:26:53 +0100

    first commit

---
+++ db_stuff.sql
@@ -0,0 +1,18 @@
+create database auth_site;
+
+create table users (
+  	id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  	email varchar(319) NOT NULL,
+ 	pass varchar(255) NOT NULL,
+   	type enum("user", "admin", "prisoner") DEFAULT "prisoner"
+);
+
+
+-- ADMIN USER
+INSERT INTO users(id, email, pass, type) VALUES (NULL,'admin@admin.fr','admin98',"admin");
+
+-- BASIC USER
+INSERT INTO users(id, email, pass, type) VALUES (NULL,'tomy@hotmail.fr','produskate89',"user");
+
+-- PRSIONER USER
+INSERT INTO users(id, email, pass, type) VALUES (NULL,'groscaca@merde.fr','mangemerde09');
